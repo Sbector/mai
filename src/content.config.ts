5ba4431fc45bdb6d6f4a6a89fd4cc5e75c222a56
No_otra_vez <saulbector@gmail.com>
2026-01-14 18:10:02 -0600

    pages cms intetration

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,31 +1,39 @@
-import { defineCollection, reference, z } from "astro:content"
-import { glob, file } from 'astro/loaders';
+import { defineCollection, reference, z } from "astro:content";
+import { glob, file } from "astro/loaders";
 
 const epistolas = defineCollection({
-    loader: glob({ pattern: '**/*.md', base: './src/data/epistolas' }),
-    schema:
-        ({ image }) => z.object({
-            title: z.string(),
-            description: z.string().optional(),
-            date: z.coerce.date(),
-            cover: image(),
-            coverAlt: z.string(),
-            itemCollection: reference("itemCollections")
-        }),
+    loader: glob({
+        pattern: "**/*.md",
+        base: "./src/data/epistolas",
+    }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        date: z.coerce.date(),
+
+        // Ruta a imagen en /public
+        cover: z.string(),
+        coverAlt: z.string(),
+
+        // Relación con itemCollections
+        itemCollection: reference("itemCollections").optional(),
+    }),
 });
 
 const itemCollections = defineCollection({
-    loader: file("./src/data/itemCollections.json"),
-    schema:
-        ({ image }) => z.object({
-            title: z.string(),
-            description: z.string(),
-            images: z.array(z.object({
-                title: z.string(),
-                description: z.string(),
-                imageRoute: image(),
-            })),
-        })
-})
+  loader: file("./src/data/itemCollections.json"),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    images: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        // Ruta absoluta desde /public
+        imageRoute: z.string(),
+      }),
+    ),
+  }),
+});
 
 export const collections = { epistolas, itemCollections }
